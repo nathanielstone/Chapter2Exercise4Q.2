b785d1e2db562b714365681ce7c0562fd0234174
Nathaniel <nathan@stone.tc>
2016-02-15 10:25:59 -0600

    Initial Commit

---
+++ Chapter2Exercise4Q.2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chapter2Exercise4Q.2
+//
+//  Created by Nathaniel Stone on 2/15/16.
+//  Copyright © 2016 Nathaniel Stone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
